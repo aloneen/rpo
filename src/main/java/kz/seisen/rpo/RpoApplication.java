13ed4d8f31e50b09e2e642ed6a02423a07adc5e7
aloneen <seisenbek2005@mail.ru>
2025-12-02 12:54:54 +0500

    First Init Commit

---
+++ src/main/java/kz/seisen/rpo/RpoApplication.java
@@ -0,0 +1,13 @@
+package kz.seisen.rpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/seisen/rpo/RpoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seisen.rpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
